8ba4f0875d6aa684b23064d9d4e14e43efbde33b
roukell <62136980+roukell@users.noreply.github.com>
2020-10-26 21:45:37 +1100

    restructured authProvider

--- client/src/pages/Admin/authProvider.js
+++ client/src/pages/Admin/authProvider.js
@@ -1,31 +1,29 @@
-import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_CHECK } from 'react-admin';
-
-export default (type, params) => {
-    // when a user tries to log in 
-    if (type === AUTH_LOGIN) {
-        const { username } = params;
-        localStorage.setItem('username', username)
+export default {
+    // called when the user attempts to log in
+    login: ({ username }) => {
+        localStorage.setItem('username', username);
+        // accept all username/password combinations
         return Promise.resolve();
-    }
-    // when a user tries to logout
-    if (type === AUTH_LOGOUT) {
-        localStorage.removeItem('username')
+    },
+    // called when the user clicks on the logout button
+    logout: () => {
+        localStorage.removeItem('username');
         return Promise.resolve();
-    }
-    // when the API throws an error
-    if (type === AUTH_ERROR) {
-        const { status } = params;
+    },
+    // called when the API returns an error
+    checkError: ({ status }) => {
         if (status === 401 || status === 403) {
             localStorage.removeItem('username');
-            return Promise.reject()
+            return Promise.reject();
         }
-        return Promise.resolve()
-    }
-    // when a user navigates to a new location
-    if (type === AUTH_CHECK) {
-        return localStorage.getItem('username') ?
-            Promise.resolve() :
-            Promise.reject();
-    }
-    return Promise.reject('Unknown Method');
+        return Promise.resolve();
+    },
+    // called when the user navigates to a new location, to check for authentication
+    checkAuth: () => {
+        return localStorage.getItem('username')
+            ? Promise.resolve()
+            : Promise.reject();
+    },
+    // called when the user navigates to a new location, to check for permissions / roles
+    getPermissions: () => Promise.resolve(),
 };
