413ba7db511641bb3b2017ce5a2cf220ad8bebab
roukell <62136980+roukell@users.noreply.github.com>
2020-10-27 20:13:22 +1100

    adding admin acount - temp change

--- routes/user.js
+++ routes/user.js
@@ -2,37 +2,37 @@ const User = require('../models/user');
 const passport = require('passport');
 const router = require("express").Router();
 
-router.post('/signup', (req, res, next) => {
-    User.register(new User({
-        username: req.body.username,
-        email: req.body.email,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName
-    }),
-        req.body.password, (err, user) => {
-            if (err) {
-                console.log(err);
-                res.statusCode = 500;
-                res.setHeader('Content-Type', 'application/json');
-                res.json({
-                    err: err
-                });
-            } else {
-                passport.authenticate('user-local')(req, res, () => {
-                    User.findOne({
-                        username: req.body.username
-                    }, (err, person) => {
-                        res.statusCode = 200;
-                        res.setHeader('Content-Type', 'application/json');
-                        res.json({
-                            success: true,
-                            status: 'Registration Successful!',
-                        });
-                    });
-                })
-            }
-        })
-});
+// router.post('/signup', (req, res, next) => {
+//     User.register(new User({
+//         username: req.body.username,
+//         email: req.body.email,
+//         firstName: req.body.firstName,
+//         lastName: req.body.lastName
+//     }),
+//         req.body.password, (err, user) => {
+//             if (err) {
+//                 console.log(err);
+//                 res.statusCode = 500;
+//                 res.setHeader('Content-Type', 'application/json');
+//                 res.json({
+//                     err: err
+//                 });
+//             } else {
+//                 passport.authenticate('user-local')(req, res, () => {
+//                     User.findOne({
+//                         username: req.body.username
+//                     }, (err, person) => {
+//                         res.statusCode = 200;
+//                         res.setHeader('Content-Type', 'application/json');
+//                         res.json({
+//                             success: true,
+//                             status: 'Registration Successful!',
+//                         });
+//                     });
+//                 })
+//             }
+//         })
+// });
 
 router.post('/login', passport.authenticate('user-local'), (req, res) => {
     User.findOne({
