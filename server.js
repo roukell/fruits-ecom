a3df9a4951b51179a32bf3ee60f5428432f71b52
roukell <62136980+roukell@users.noreply.github.com>
2020-10-27 21:49:37 +1100

    update server.js

--- server.js
+++ server.js
@@ -57,13 +57,14 @@ mongoose.connect(
   }
 );
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get('*',  (req,res) =>{
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get('*',  (req,res) =>{
+//     res.sendFile(path.join(__dirname + '/client/build/index.html'));
+// });
+// }
 
+app.get('*', res => res.sendFile(path.join(__dirname + '/client/build/index.html')));
 // Start the API server
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
