f705169c627bdde2a6de044809b15db44297437b
roukell <62136980+roukell@users.noreply.github.com>
2020-11-01 19:24:08 +1100

    added tabs for orderList

--- client/src/pages/Admin/OrderList.js
+++ client/src/pages/Admin/OrderList.js
@@ -8,6 +8,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import { Divider, Tabs, Tab } from '@material-ui/core';
 
 const useStyles = makeStyles({
     table: {
@@ -15,51 +16,81 @@ const useStyles = makeStyles({
     },
 });
 
-const OrderList = props => {
-    const PostFilter = (props) => (
-        <Filter {...props}>
-            <TextInput label="Search" source="q" alwaysOn />
-        </Filter>
-    );
+// filter box function
+const PostFilter = (props) => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+    </Filter>
+);
 
-    const PostPanel = ({ id, record, resource }) => {
-        const classes = useStyles();
+// expand panel to show order details
+const PostPanel = ({ id, record, resource }) => {
+    const classes = useStyles();
 
-        const orderItems = record.orders.map((order, i) => {
-            const title = order.title;
-            const quantity = order.quantity;
-            console.log(title, quantity);
-
-            return (
-                <TableRow key={i}>
-                    <TableCell align="center">{title}</TableCell>
-                    <TableCell align="center">{quantity}</TableCell>
-                </TableRow>
-            )
-        })
+    const orderItems = record.orders.map((order, i) => {
+        const title = order.title;
+        const quantity = order.quantity;
+        console.log(title, quantity);
 
         return (
-            <>
-                <h3>Details</h3>
-                <TableContainer component={Paper}>
-                    <Table className={classes.table} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell align="center">Product title</TableCell>
-                                <TableCell align="center">Quantity</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {orderItems}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </>
+            <TableRow key={i}>
+                <TableCell align="center">{title}</TableCell>
+                <TableCell align="center">{quantity}</TableCell>
+            </TableRow>
         )
-    }
+    })
 
     return (
-        <List {...props} filters={<PostFilter />}>
+        <>
+            <h3>Details</h3>
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center">Product title</TableCell>
+                            <TableCell align="center">Quantity</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {orderItems}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+    )
+}
+
+// tabs for different order status
+const tabs = [
+    { id: 'ordered', name: 'ordered' },
+    { id: 'delivered', name: 'delivered' },
+    { id: 'cancelled', name: 'cancelled' },
+];
+
+const TabbedDatagrid = () => {
+    return (
+        <>
+            <Tabs
+                variant="fullWidth"
+                centered
+                // value={filterValues.status}
+                indicatorColor="primary"
+            // onChange={handleChange}
+            >
+                {tabs.map(choice => (
+                    <Tab
+                        key={choice.id}
+                        label={
+                            // totals[choice.name]
+                            //     ? `${choice.name} (${totals[choice.name]})`
+                            //     : 
+                            choice.name
+                        }
+                        value={choice.id}
+                    />
+                ))}
+            </Tabs>
+            <Divider />
             <Datagrid rowClick="edit" expand={<PostPanel />}>
                 <TextField source="date" />
                 <TextField source="_id" />
@@ -70,7 +101,17 @@ const OrderList = props => {
                     </SingleFieldList>
                 </ArrayField>
             </Datagrid>
-        </List>
+        </>
+    )
+}
+
+const OrderList = props => {
+    return (
+        <>
+            <List {...props} filters={<PostFilter />}>
+                <TabbedDatagrid />
+            </List>
+        </>
     )
 };
 
