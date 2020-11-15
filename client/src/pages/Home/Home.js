cc1cca64ca545bcc22b6916eb3846e666f6b2496
roukell <62136980+roukell@users.noreply.github.com>
2020-11-15 19:39:57 +1100

    removed alert test

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
     return(
         <>
         <Navigation items={customerNavbarItems} />  
-        <Alert />
+        {/* <Alert /> */}
         <Carousel />
         <h3>
         BEST SELLERS
